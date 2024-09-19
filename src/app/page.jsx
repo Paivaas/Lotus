2164f5af7e8022cc231754527a6190c8c137d34c
JULIA PAIVA DE OLIVEIRA <23111736@dsn127m1249386.cfp127aluno.local>
2024-09-19 17:00:12 -0300

    :wastebasket: feat: Tela de Login (responsividade e tratativa de erro :white_check_mark: )

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,93 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
+import {  Modal,   ModalContent,   ModalHeader,   ModalBody,   ModalFooter} from "@nextui-org/react";
+
+// Import das imagens 
+import LotusIcon from "../../public/icons/utilities/lotus-icon.svg";
+import EmailIcon from "../../public/icons/profile-information/grey/email.svg"
+import KeyIcon from "../../public/icons/profile-information/grey/key.svg"
+import ArrowIcon from "../../public/icons/utilities/arrow-white.svg"
 
 export default function Home() {
-  return (
-    <div className="font-ABeeZee bg-red-degrade-1 font-inter grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 
-      
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const validacaoLogin = (e) => {
+    e.preventDefault();
+    setError(""); 
+
+    // Validacao e tratativas de erro
+
+    if (email == "" || password == "" || !email.includes('@') || !email.includes('.com')) {
+      console.log("Por favor, preencha todos os campos.");
+      return;
+    }
+    console.log("Login realizado com sucesso");
+  };
+  
+  return (
+    
+    <div className="flex w-screen h-screen max-xl:p-16 max-sm:p-8">
+
+      <div className="h-full w-2/3 flex flex-col justify-center items-center gap-20 max-xl:w-full">
+
+        <div className="flex flex-col gap-14">
+          
+          <div className="flex flex-col gap-4 pb-10">
+            <Image className="w-[15%]" alt="Lotus Icon" src={LotusIcon}></Image>
+
+            <h1 className="text-gray-3 text-5xl">Login Lótus</h1>
+            <h2 className="text-gray-3">Ainda não possui uma conta? <span className="hover:text-gray-2 cursor-pointer">Clique aqui</span> </h2>
+          </div>
+
+          {/* Campos para entrada de valor */}
+
+          <form className="flex flex-col gap-4 w-[40vw] font-ABeeZee max-xl:w-full">
+
+            <div className="flex p-4 rounded-3xl border-[3px] bg-white gap-4">
+                <Image className="w-[5%] max-sm:w-[10%]" alt="Email Icon" src={EmailIcon}></Image>
+                <input type="email" placeholder="Seu email" className="w-full" onChange={(e) => setEmail(e.target.value)}
+                aria-label="Seu email"
+                required/>
+              </div>
+
+              <div className="flex p-4 rounded-3xl border-[3px] bg-white gap-4">
+                <Image className="w-[5%] max-sm:w-[10%]" alt="Key Icon" src={KeyIcon}></Image>
+                <input type="password" placeholder="Sua Senha" className="w-full" onChange={(e) => setPassword(e.target.value)}aria-label="Sua Senha"
+                required/>
+              </div>
+
+              <p className="text-pink-3 hover:text-pink-2  hover:cursor-pointer">Esqueceu sua senha?</p>
+
+          </form>
+
+          <button onClick={validacaoLogin} className="bg-gradient-to-r from-pink-3 to-orange-3 w-40 p-4 px-6 items-center justify-between rounded-full text-white flex hover:cursor-pointer hover:scale-95">
+            
+            <p className="text-xl">Entrar</p>
+            <Image className="w-[20%]" alt="Arrow Icon" src={ArrowIcon}></Image>
+            
+          </button>
+
+        </div>
+
+      </div>
+
+
+      <div className="h-full w-1/3 max-xl:w-0 max-xl:hidden">
+
+        <div className="w-full h-1/3 bg-red-degrade-1 flex align-top justify-end"></div>
+        <div className="w-full h-1/3 bg-red-degrade-2"></div>
+        <div className="w-full h-1/3 bg-red-degrade-3"></div>
+
+      </div>
       
     </div>
   );
 }
+
