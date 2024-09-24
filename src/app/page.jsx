54b13176c5617e889c5a1d0b337ac937a7d9231a
natybastazini <natybastazini7@gmail.com>
2024-09-24 11:43:18 -0300

    precisa realizar o js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -13,8 +13,23 @@ import LogoChat from "../../public/icons/nav/chat.svg"
 import LogoGaleria from "../../public/icons/nav/galeria.svg"
 import LogoPerfil from "../../public/icons/nav/profile.svg"
 
-
 export default function Home() {
+
+  // const [cardsData, setCardsData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchCardsData = async () => {
+  //     try {
+  //       const response = await axios.get("URL_DA_SUA_API");
+  //       setCardsData(response.data); // Assumindo que os dados estão no formato correto
+  //     } catch (error) {
+  //       console.error("Erro ao buscar os dados:", error);
+  //     }
+  //   };
+
+  //   fetchCardsData();
+  // }, []) ;
+
   return (
     <div className="flex h-screen">
       <header className="flex flex-col w-[20%] py-10 px-10">
@@ -27,56 +42,56 @@ export default function Home() {
         </div>
         {/* navegação */}
         <nav className="flex flex-col gap-10 grow">
-          <div className="flex flex-row items-center gap-2">
-            <Image src={LogoHome} alt="home" className="size-12" ></Image>
+          <button className="flex flex-row items-center p-2 gap-2 hover:bg-pink-degrade-1 rounded-xl hover:text-white">
+            <Image src={LogoHome} alt="home" className="size-8" ></Image>
             <h1 className="font-Inter font-normal text-gray-2 text-lg">
               Home
             </h1>
-          </div>
-          <div className="flex flex-row items-center gap-2">
-            <Image src={LogoMonitoramento} alt="monitoramento" className="size-12" ></Image>
+          </button>
+          <button className="flex flex-row items-center p-2 gap-2 hover:bg-pink-degrade-1 rounded-xl">
+            <Image src={LogoMonitoramento} alt="monitoramento" className="size-8" ></Image>
             <h1 className="font-Inter font-normal text-gray-2 text-lg">
               Monitoramento
             </h1>
-          </div>
-          <div className="flex flex-row items-center gap-2">
-            <Image src={LogoConteudo} alt="conteudo" className="size-12" ></Image>
+          </button>
+          <button className="flex flex-row items-center p-2 gap-2 hover:bg-pink-degrade-1 rounded-xl">
+            <Image src={LogoConteudo} alt="conteudo" className="size-8" ></Image>
             <h1 className="font-Inter font-normal text-gray-2 text-lg">
               Conteúdo
             </h1>
-          </div>
-          <div className="flex flex-row items-center gap-2">
-            <Image src={LogoChat} alt="chat" className="size-12" ></Image>
+          </button>
+          <button className="flex flex-row items-center p-2 gap-2 hover:bg-pink-degrade-1 rounded-xl">
+            <Image src={LogoChat} alt="chat" className="size-8" ></Image>
             <h1 className="font-Inter font-normal text-gray-2 text-lg">
               Chat
             </h1>
-          </div>
-          <div className="flex flex-row items-center gap-2">
-            <Image src={LogoGaleria} alt="galeria" className="size-12" ></Image>
+          </button>
+          <button className="flex flex-row items-center p-2 gap-2 hover:bg-pink-degrade-1 rounded-xl">
+            <Image src={LogoGaleria} alt="galeria" className="size-8" ></Image>
             <h1 className="font-Inter font-normal text-gray-2 text-lg">
               Galeria
             </h1>
-          </div>
-          <div className="flex flex-row items-center gap-2">
-            <Image src={LogoPerfil} alt="perfil" className="size-12" ></Image>
+          </button>
+          <button className="flex flex-row items-center p-2 gap-2 hover:bg-pink-degrade-1 rounded-xl">
+            <Image src={LogoPerfil} alt="perfil" className="size-8" ></Image>
             <h1 className="font-Inter font-normal text-gray-2 text-lg">
               Perfil
             </h1>
-          </div>
+          </button>
         </nav>
         {/* botão sair */}
-        <div className="flex flex-row gap-2 items-center">
+        <button className="flex flex-row gap-2 items-center">
           <Image src={LogoLogout} alt="logout" className="size-8"></Image>
           <p className="font-Inter font-normal text-gray-2 text-lg">
             Log out
           </p>
-        </div>
+        </button>
       </header>
-      <main className="grow bg-white p-10">
+      <main className="w-[80%] bg-white p-10">
         {/* conteúdo */}
-        <div className="bg-gray-1 w-full h-full rounded-[40px]">
+        <div className="bg-gray-1 w-full h-full rounded-[40px] overflow-hidden">
           {/* degradê */}
-          <div className="bg-pink-degrade-3 flex flex-row w-full h-28 justify-end rounded-s-[40px]">
+          <div className="bg-pink-degrade-3 flex flex-row w-full h-28 justify-end rounded-s-[40px] rounded-e-[40px] overflow-hidden">
             <div className="bg-pink-degrade-2 w-2/3 h-28 flex justify-end rounded-b-full">
               <div className="bg-pink-degrade-1 w-1/2 h-28 rounded-bl-full"></div> 
             </div> 
@@ -98,15 +113,17 @@ export default function Home() {
                 <h1 className="font-ABeeZee font-medium text-3xl text-gray-3 ">
                   Amamentação
                 </h1>
-                <div className="flex flex-row items-center gap-2">
-                  <Image src={LogoVerMais} alt="ver-mais" className="size-4"></Image>
-                  <a className="font-Inter font-light text-base text-gray-3">Ver mais</a>
-                </div>
+                <button>
+                  <div className="flex flex-row items-center gap-2">
+                    <Image src={LogoVerMais} alt="ver-mais" className="size-4"></Image>
+                    <a className="font-Inter font-light text-base text-gray-3">Ver mais</a>
+                  </div>
+                </button>
               </div>
               {/* card */}
               <div className="flex flex-row gap-8">
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
-                  <div className="h-[80%] rounded-2xl bg-pink-300">
+                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg aspect-video">
+                  <div className="h-[80%] rounded-2xl bg-pink-300 ">
                     <div className="h-full w-full">
                       <Image></Image>          
                     </div>
@@ -116,32 +133,9 @@ export default function Home() {
                     Desvendando a Amamentação
                   </p>
                 </div>
+                
                 {/* card */}
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
-                  <div className="h-[80%] rounded-2xl bg-pink-300">
-                    <div className="h-full w-full">
-                      <Image></Image>
-                    </div>
-                  </div>
-                  {/* título do card */}
-                  <p className="font-Inter font-medium text-xl text-gray-2 p-4">
-                    Desvendando a Amamentação
-                  </p>
-                </div>
-                {/* card */}
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
-                  <div className="h-[80%] rounded-2xl bg-pink-300">
-                    <div className="h-full w-full">
-                      <Image></Image>
-                    </div>
-                  </div>
-                  {/* título do card */}
-                  <p className="font-Inter font-medium text-xl text-gray-2 p-4">
-                    Desvendando a Amamentação
-                  </p>
-                </div>
-                {/* card */}
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
+                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg aspect-video">
                   <div className="h-[80%] rounded-2xl bg-pink-300">
                     <div className="h-full w-full">
                       <Image></Image>
@@ -162,14 +156,16 @@ export default function Home() {
                 <h1 className="font-ABeeZee font-medium text-3xl text-gray-3 ">
                   Amamentação
                 </h1>
-                <div className="flex flex-row items-center gap-2">
-                  <Image src={LogoVerMais} alt="ver-mais" className="size-4"></Image>
-                  <a className="font-Inter font-light text-base text-gray-3">Ver mais</a>
-                </div>
+                <button>
+                  <div className="flex flex-row items-center gap-2">
+                    <Image src={LogoVerMais} alt="ver-mais" className="size-4"></Image>
+                    <a className="font-Inter font-light text-base text-gray-3">Ver mais</a>
+                  </div>
+                </button>
               </div>
               {/* card */}
               <div className="flex flex-row gap-8">
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
+                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg aspect-video">
                   <div className="h-[80%] rounded-2xl bg-pink-300">
                     <div className="h-full w-full">
                       <Image></Image>
@@ -180,8 +176,7 @@ export default function Home() {
                     Desvendando a Amamentação
                   </p>
                 </div>
-                {/* card */}
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
+                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg aspect-video">
                   <div className="h-[80%] rounded-2xl bg-pink-300">
                     <div className="h-full w-full">
                       <Image></Image>
@@ -192,30 +187,7 @@ export default function Home() {
                     Desvendando a Amamentação
                   </p>
                 </div>
-                {/* card */}
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
-                  <div className="h-[80%] rounded-2xl bg-pink-300">
-                    <div className="h-full w-full">
-                      <Image></Image>
-                    </div>
-                  </div>
-                  {/* título do card */}
-                  <p className="font-Inter font-medium text-xl text-gray-2 p-4">
-                    Desvendando a Amamentação
-                  </p>
-                </div>
-                {/* card */}
-                <div className="h-72 w-[500px] bg-white rounded-2xl shadow-lg">
-                  <div className="h-[80%] rounded-2xl bg-pink-300">
-                    <div className="h-full w-full">
-                      <Image></Image>
-                    </div>
-                  </div>
-                  {/* título do card */}
-                  <p className="font-Inter font-medium text-xl text-gray-2 p-4">
-                    Desvendando a Amamentação
-                  </p>
-                </div>
+                
               </div>
             </div>
 
